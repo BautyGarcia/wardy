951fbf8946cc2f0fc48663bf2a0a712188477a49
Bauty Garcia <bautistaroccogarcia@gmail.com>
2025-03-04 23:00:37 -0300

    test

--- lib/features/wardrobe/screens/wardrobe_screen.dart
+++ lib/features/wardrobe/screens/wardrobe_screen.dart
@@ -366,8 +366,20 @@ class _WardrobeScreenState extends State<WardrobeScreen> {
                                       secondaryAnimation,
                                       child,
                                     ) {
-                                      return FadeTransition(
-                                        opacity: animation,
+                                      const begin = Offset(1.0, 0.0);
+                                      const end = Offset.zero;
+                                      const curve = Curves.easeInOut;
+
+                                      var tween = Tween(
+                                        begin: begin,
+                                        end: end,
+                                      ).chain(CurveTween(curve: curve));
+                                      var offsetAnimation = animation.drive(
+                                        tween,
+                                      );
+
+                                      return SlideTransition(
+                                        position: offsetAnimation,
                                         child: child,
                                       );
                                     },
